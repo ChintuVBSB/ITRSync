50ba57891202529ee9fccbdfda36c7e8d44b6fe5
Chintu Jha <chintu.jha@vbsb.in>
2025-06-05 12:03:31 +0530

    final income ui discussed with team

--- app/Models/IncomeType.php
+++ app/Models/IncomeType.php
@@ -3,14 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class IncomeType extends Model
 {
     //
     protected $fillable = ['slug', 'label'];
 
-    public function submissions()
+    public function submission(): BelongsTo
     {
-        return $this->belongsToMany(Submission::class);
+    return $this->belongsTo(Submission::class);
     }
 }
